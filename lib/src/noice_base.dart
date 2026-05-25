1b6afcfeea9049f160f653fbf04cf8cd1c3f4c53
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2026-05-25 11:52:53 +0200

    feat(DM-CI-213): logout method (#9)
    logout method

--- lib/src/noice_base.dart
+++ lib/src/noice_base.dart
@@ -6,4 +6,9 @@ class Awesome {
     await Future.delayed(Duration(seconds: 1));
     return username == 'admin' && password == 'admin';
   }
+
+  // Always works.
+  Future<bool> logout() {
+    return Future.value(true);
+  }
 }
