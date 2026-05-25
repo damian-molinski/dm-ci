63443dd82ccdb4a1eeb628378ca22339c1bce990
Damian Molinski <d.molinski@fivedottwelve.com>
2026-05-25 11:47:51 +0200

    implement auth method

--- lib/src/noice_base.dart
+++ lib/src/noice_base.dart
@@ -1,3 +1,9 @@
 class Awesome {
   bool get isAwesome => true;
+
+  // Super secure authentication method.
+  Future<bool> auth(String username, String password) async {
+    await Future.delayed(Duration(seconds: 1));
+    return username == 'admin' && password == 'admin';
+  }
 }
